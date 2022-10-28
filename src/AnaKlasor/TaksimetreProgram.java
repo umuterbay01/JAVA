b970684ee03c59411867554ace029d24c99296b0
umuterbay01 <umuterbayy135@gmail.com>
2022-10-28 13:24:35 +0300

    KDV HESAPLAMA

--- src/AnaKlasor/TaksimetreProgram.java
+++ src/AnaKlasor/TaksimetreProgram.java
@@ -4,13 +4,14 @@ import java.util.Scanner;
 
 public class TaksimetreProgram {
     public static void main(String[] args) {
-        int baslangic=10;
-        double tutar, km;
+        int km;
+        double tutar, baslangic = 10;
         Scanner tks = new Scanner(System.in);
         System.out.println("TAKSİMETRE AÇILIŞ : " + baslangic);
         System.out.print("KAÇ KİLOMETRE GİTTİĞİNİZİ GİRİNİZ :");
-        km = tks.nextDouble();
-        tutar = (2.2 * km) + 10;
+        km = tks.nextInt();
+        tutar = (2.20 * km);
+        tutar += baslangic;
         boolean kosul1 = tutar >= 20.0;
         double enson = kosul1 ? tutar : 20.0;
         System.out.println(enson + "TL");
